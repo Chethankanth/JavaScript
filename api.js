ed9e10631e10590584b84af816a36c91128f4f7e
Chethankanth <chethanga4@gmail.com>
2025-03-14 22:11:07 +0530

    currency project added

--- api.js
+++ api.js
@@ -1,12 +1,20 @@
 
-const URL = "https://github.com/facts";
+const URL = "https://official-joke-api.appspot.com/random_ten";
+
+let msg = document.querySelector(".myclass");
+const btn = document.querySelector("#mbutton");
+
+
 
 const getFacts = async ()=>{
     console.log("fetchig the data!..");
-    let response = await fetch(URL);
+    let response = await fetch(URL);//fetch api calls and fetches the data;
     console.log("data fetched");
-    console.log(response)
-    let data = response.json();
-    console.log(data);
+    console.log(response)//gets response
+    let data = response.json();//json mekes it to redable formate
+    msg.innerText = data[0];
+
 
 }
+
+btn.addEventListener("click",getFacts());
