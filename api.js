b2e4c8fccfd4a2ba1b24d45462a5fd3513c6f8bd
Chethankanth <chethanga4@gmail.com>
2025-03-14 21:08:48 +0530

    new change

--- api.js
+++ api.js
@@ -1,10 +1,12 @@
 
-const URL = "https://cat-fact.herokuapp.com/facts";
+const URL = "https://github.com/facts";
 
 const getFacts = async ()=>{
     console.log("fetchig the data!..");
     let response = await fetch(URL);
     console.log("data fetched");
     console.log(response)
+    let data = response.json();
+    console.log(data);
 
 }
